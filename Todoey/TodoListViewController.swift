4ef7b8de58322b372fe98799769481095a1f5512
Yi Huang <yihuang@Yis-MacBook-Pro.local>
2023-03-10 14:34:36 -0800

    initial commit
    Signed-off-by: Yi Huang <yihuang@Yis-MacBook-Pro.local>

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Grocery Shopping", "Go to dym", "Feed cat"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK - Tableview datasource mathods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+
+
+}
+
+
